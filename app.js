f23a3b1b1a6cd133cb04a19e2fd1c6f259557938
temporary user <temporaryuser@int-mac-25.domain.name>
2023-08-25 00:38:28 +0530

    counter example

---
+++ app.js
@@ -0,0 +1,39 @@
+const rootNode = document.getElementById("main");
+const root = ReactDOM.createRoot(rootNode);
+
+root.render(React.createElement(App));
+
+let counter = 1;
+
+function App() {
+  return React.createElement(
+    "div",
+    null,
+    React.createElement(
+      "h2",
+      null,
+      React.createElement(Component, { name: "This is a Header" }),
+      counter % 2 === 0
+        ? React.createElement(Counter, { count: counter })
+        : React.createElement(Counter2, { count: counter })
+    )
+  );
+}
+
+function Component(props) {
+  return React.createElement("p", null, props.name);
+}
+
+function Counter(props) {
+  return React.createElement("p", null, "Count: even ", props.count);
+}
+
+function Counter2(props) {
+  return React.createElement("p", null, "Count: odd ", props.count);
+}
+
+function update() {
+  console.log("clicked update..");
+  counter += 1;
+  root.render(React.createElement(App));
+}
---
+++ readme.md
@@ -0,0 +1,5 @@
+### NOTES : 
+Props are immutable. `props` Object is frozen. But after destructuring it we can mutate them
+
+
+<!-- completed_timestamp : 2:16 -->
